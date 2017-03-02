d4559da64ca89774af256b99af1638184176cffc
rebeccabuie <rebeccabuie@gmail.com>
2017-03-02 13:40:37 -0700

    Pushing initial files

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent} from './app.component';
+import { HomepageComponent} from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
+
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+
+  ],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    DocumentsComponent,
+    ProposalListComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
---
+++ app/documents/documents.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Document } from './document';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+export class DocumentsComponent {
+  pageTitle: string = "Document Dashboard"
+  
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: 'asdfsdgf asdfgasdg asdgfsad asd',
+      file_url: 'http://google.com',
+      updated_at: '03/02/2016',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Second Doc",
+      description: 'asdfsdgf asdfgasdg asdgfsad asd',
+      file_url: 'http://google.com',
+      updated_at: '03/02/2016',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Last Doc",
+      description: 'asdfsdgf asdfgasdg asdgfsad asd',
+      file_url: 'http://google.com',
+      updated_at: '03/02/2016',
+      image_url: 'http://google.com',
+    }
+  ]
+}
